835ab5ea0508672fd9d26f20b1348091c57a9baa
pedro Okuma <pedrookuma3@gmail.com>
2020-11-16 23:36:40 -0300

    Projeto Calculadora simples realizado

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace CalculadoraSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual operação deseja realizar? \n Soma, Subtacao, Multiplicacao, Divisao");
+            string operacao = Console.ReadLine().ToLower();
+
+            Console.WriteLine("Digite o 1° número : ");
+            float num1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o 2° número : ");
+            float num2 = float.Parse(Console.ReadLine());
+
+            float resultado = 0;
+
+            switch (operacao){
+                case "soma":
+                    resultado = num1 + num2;
+                    break;
+
+                case "subtracao":
+                    resultado = num1 - num2;
+                    break;
+
+                case "multiplicacao":
+                    resultado = num1 * num2;
+                    break;
+
+                case "divisao":
+                    resultado = num1 / num2;
+                    break;
+
+                default:
+                    operacao = "invalida";
+                    Console.WriteLine("Operação Inválida! :(");
+                    break;
+            }
+            if(operacao != "invalida")
+                Console.WriteLine($"Calculo: {num1} com {num2} = {resultado}");            
+        }
+    }
+}
